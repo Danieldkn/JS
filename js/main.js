47af1149aefb7baa93fc433ffededa24f9d0585b
Daniel Ramirez <danyeldkn@gmail.com>
2022-08-31 02:30:22 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,70 @@
+const arrayProductos = productos;
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    renderizarHtml(arrayProductos);
+
+    const nombreProducto = document.querySelector('#nombre-producto');
+
+    nombreProducto.addEventListener('input', () => {
+        const encontrados = arrayProductos.filter( ( { nombre } ) => {
+            return nombre.toUpperCase().includes(nombreProducto.value.toUpperCase());
+        });
+        renderizarHtml(encontrados);
+    });
+
+    const filtrar = document.querySelector('#filtrar');
+
+    filtrar.addEventListener('click', () => {
+        const desde = document.querySelector('#desde').value;
+        const hasta = document.querySelector('#hasta').value;
+
+        const encontrados = arrayProductos.filter( ( { precio } ) => {
+            return  Number(precio) >= desde && Number(precio) <= hasta;
+        });
+        renderizarHtml(encontrados);
+    })
+
+
+})
+
+
+function renderizarHtml(array){
+    const tbody = document.querySelector('tbody');
+
+    tbody.innerHTML = '';
+
+    if(array.length === 0){
+        tbody.innerHTML = "<h1 class='mt-5'>No se encontraron resultados</h1>"
+    }
+
+    array.forEach( ( { imagen, nombre, precio } ) => {
+        const tr = document.createElement('tr');  
+        tr.innerHTML = `<td><img src="${imagen}"></td>
+                        <td>${nombre}</td>
+                        <td>$${precio}</td>
+                        <button class="boton-comprar">Agregar</button>`
+
+        
+        tbody.appendChild(tr);
+    });
+}
+
+document.getElementById("btnCancelarCompra").onclick = () => {
+    Swal.fire({
+        title: 'Borrar productos?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Borrar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+        Swal.fire(
+            'Limpio!',
+            'Tu carrito esta vacio',
+            'success'
+        )
+        }
+    })
+}
---
+++ js/productos.js
@@ -0,0 +1,74 @@
+const productos = [
+    {
+        nombre: "La busqueda",
+        precio: 950,
+        imagen: "./img/libro1.png",
+        id: 1
+    },
+    {
+        nombre: "El armario",
+        precio: 750,
+        imagen: "./img/libro2.png",
+        id: 2
+    },
+    {
+        nombre: "El loco",
+        precio: 1000,
+        imagen: "./img/libro3.png",
+        id: 3
+    },
+    {
+        nombre: "La mañana",
+        precio: 100,
+        imagen: "./img/libro4.png",
+        id: 4
+    },
+    {
+        nombre: "Sueño",
+        precio: 900,
+        imagen: "./img/libro5.png",
+        id: 5
+    },
+    {
+        nombre: "Torcedura",
+        precio: 400,
+        imagen: "./img/libro6.png",
+        id: 6
+    },
+    {
+        nombre: "Terror",
+        precio: 800,
+        imagen: "./img/libro7.png",
+        id: 7
+    },
+    {
+        nombre: "El ancla",
+        precio: 700,
+        imagen: "./img/libro8.png",
+        id: 8
+    },
+    {
+        nombre: "La noche",
+        precio: 500,
+        imagen: "./img/libro9.png",
+        id: 9
+    },
+    {
+        nombre: "El buscador",
+        precio: 800,
+        imagen: "./img/libro10.png",
+        id: 10
+    },
+    {
+        nombre: "Lugares Increibleas",
+        precio: 500,
+        imagen: "./img/libro11.png",
+        id: 11
+    },
+    {
+        nombre: "La Monarquia",
+        precio: 200,
+        imagen: "./img/libro12.png",
+        id: 12
+    },
+]
